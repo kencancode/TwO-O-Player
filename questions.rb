a5aab427bae40eb14787fc35e67d1e891dd9a7ed
Ken Thooptong <ken.thooptong@gmail.com>
2019-03-19 00:13:30 +0000

    All game logic in one file. For one player only

--- questions.rb
+++ questions.rb
@@ -1,20 +1,27 @@
-class Questions
+class Game
   
     def initialize(current_player)
-      @num1 = rand(20)
-      @num2 = rand(20)
+      @lives = 3
+      @num1 = rand(5)
+      @num2 = rand(5)
       @answer = @num1 + @num2
   
+    while @lives > 0 do
       puts "#{current_player}: #{@num1} + #{@num2}?"
       
       @player_answer = gets.chomp.to_i
 
       if @answer == @player_answer.to_i
         puts "Oui, Genius!"
-        @correct = true
+        puts "Your hp is #{@lives} /3"
       else 
         puts "Oh no..."
-        @correct = false
+        @lives -= 1
+        puts "You have #{@lives} /3"
       end
     end
+    end
+
   end
+
+  test = Game.new("Player")
